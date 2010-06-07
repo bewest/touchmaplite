3539910d176d09667dbb0c059775ede07f23b9c6
rac98 <rac98@7db46829-8047-4a9b-949e-05034ab5a102>
2010-06-07 10:05:17 +0000

    recenter on center

--- js/touchMapLite/touchMapLite.geolocation.js
+++ js/touchMapLite/touchMapLite.geolocation.js
@@ -73,9 +73,18 @@ touchMapLite.prototype.nolocationFound = function(error){
 
 touchMapLite.prototype.recenterLonLat = function(position){
     var wasZoomed=false;
+
 	lon = position.coords.longitude;
 	lat = position.coords.latitude;
-	findOnMap.viewerBean.initialPan = { 'x' : findOnMap.lon2pan(lon), 'y' : findOnMap.lat2pan(lat)};
+
+	findOnMap.lon = lon;
+	findOnMap.lat = lat;
+	
+	fullSize = findOnMap.viewerBean.tileSize * Math.pow(2, findOnMap.viewerBean.zoomLevel);
+	x = Math.floor(findOnMap.lon2pan(lon)*fullSize);
+	y = Math.floor(findOnMap.lat2pan(lat)*fullSize);
+	findOnMap.viewerBean.recenter({'x':x, 'y':y}, true);
+
 	if(!watchId && position.coords.accuracy){	
 		// needs a more sophisticated technique
 		zoomLevel = 18-Math.floor(Math.log(position.coords.accuracy));
@@ -84,9 +93,6 @@ touchMapLite.prototype.recenterLonLat = function(position){
             findOnMap.viewerBean.zoomLevel = zoomLevel; 
         }
 	}
-	findOnMap.viewerBean.clear();
-	findOnMap.viewerBean.init();
-	findOnMap.viewerBean.notifyViewerMoved({x:findOnMap.viewerBean.x, y:findOnMap.viewerBean.y});
 	if(wasZoomed==true) findOnMap.viewerBean.notifyViewerZoomed();
 	if(typeof findOnMap.marker != 'undefined'){
 		var home = new findOnMap.marker('GPS',lat, lon,findOnMap,true);
